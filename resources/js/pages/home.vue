447516cc99af9abb4135a1dc942d1c066d98035c
DmatrixNg <49094365+DmatrixNg@users.noreply.github.com>
2019-10-12 08:30:56 +0100

    quick save

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,7 +1,14 @@
 <template>
   <card :title="$t('home')">
     {{ $t('you_are_logged_in') }}
+    <p>
+      Continue Profile setup
+    <router-link :to="{ name: 'profileSetup' }">
+      {{ $t('ProfileSetup') }}
+    </router-link>
+  </p>
   </card>
+
 </template>
 
 <script>
