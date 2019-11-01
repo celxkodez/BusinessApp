1241d3aff2b2f123ffcaad1b3ea3690c06e8df4e
Lord Jerry <lovinjerry004@gmail.com>
2019-11-01 16:45:45 -0700

    make a few changes to business and product schema

--- database/migrations/2019_10_16_230351_products.php
+++ database/migrations/2019_10_16_230351_products.php
@@ -15,22 +15,22 @@ class Products extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigIncrements('business_id');
+            $table->unsignedBigInteger('business_id');
             $table->string('name');
             $table->Text('short_description');
             $table->string('brand_name');
             $table->decimal('price',9,2);
             $table->integer('discount')->nullable();
             $table->string('phone_number');
-            $table->json('size')->nullable();
-            $table->json('color')->nullable();
             $table->text('product_description');
             $table->text('product_specification');
             $table->text('shipping_information');
             $table->text('return_policy_1');
             $table->text('warranty_information');
             $table->text('return_policy_2');
-            $table->json('images')->nullable();
+            // $table->json('images');
+            // $table->json('size');
+            // $table->json('color');
             $table->timestamps();
 
             $table->foreign('business_id')->references('id')->on('business');
